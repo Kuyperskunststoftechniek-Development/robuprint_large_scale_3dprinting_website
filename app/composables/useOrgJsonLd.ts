e9277ee5af02e36340f55826bbcea07d6da3831b
nickmichiels-kuypers <werkvoorbereiding@kuypers.com>
2026-05-09 16:40:28 +0200

    bedrijfsgegevens

--- app/composables/useOrgJsonLd.ts
+++ app/composables/useOrgJsonLd.ts
@@ -8,6 +8,16 @@ export function useOrgJsonLd() {
           '@type': 'Organization',
           name: 'RoBuPRINT',
           url: 'https://robuprint.nl',
+          address: {
+            '@type': 'PostalAddress',
+            streetAddress: 'Diamantweg 48',
+            postalCode: '5527 LC',
+            addressLocality: 'Hapert',
+            addressRegion: 'Noord-Brabant',
+            addressCountry: 'NL',
+          },
+          vatID: 'NL801225401B01',
+          taxID: '18036761',
           parentOrganization: {
             '@type': 'Organization',
             name: 'Kuypers Kunststoftechniek',
